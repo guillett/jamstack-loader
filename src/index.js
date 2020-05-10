d2bac1dfc52e0406da9da12b79e88f8bfcd2c374
Thomas Guillet <guillet.thomas@gmail.com>
2020-05-10 18:53:58 +0200

    Drop debug logs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const utils = require('./utils')
 
 module.exports = function loader(source) {
-  console.log('her')
   const stack = utils.parse.bind(this)(utils.load(source), this.resourcePath)
-
-  console.log(stack)
-
   return `export default ${ JSON.stringify(stack, null, 2) }`
 }
